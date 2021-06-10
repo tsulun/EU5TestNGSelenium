4b6ac4d90c3962619f2f9546772410f2ba8149a5
tsulun <tsulun16@gmail.com>
2021-06-10 23:55:15 +0100

    homework

--- src/test/java/com/cybertek/tests/homeWork/Homework_1.java
+++ src/test/java/com/cybertek/tests/homeWork/Homework_1.java
@@ -14,10 +14,21 @@ public class Homework_1 {
         System.out.println(driver.findElement(By.cssSelector("h1.srp-controls__count-heading>span")).getText());
         driver.quit();
 
+        driver=WebDriverFactory.getDriver("chrome");
         driver.get("https://www.ebay.co.uk/");
 
         driver.findElement(By.cssSelector("#gh-ac")).sendKeys("Selenium");
         driver.findElement(By.xpath("//input[@id='gh-btn']")).click();
-        driver.getTitle().equals("Selenium")
+        System.out.println(driver.getTitle().contains("Selenium"));
+        driver.close();
+
+        driver=WebDriverFactory.getDriver("chrome");
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        driver.findElement(By.cssSelector("input[name='search']")).sendKeys("selenium webdriver");
+        driver.findElement(By.cssSelector("input[name='go']")).click();
+        driver.findElement(By.cssSelector("[href='/wiki/Selenium_(software)']")).click();
+        System.out.println(driver.getCurrentUrl().endsWith("Selenium_(software)"));
+        driver.quit();
+
     }
 }
